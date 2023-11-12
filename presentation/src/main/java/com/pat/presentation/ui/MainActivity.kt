e49f0034da055ec1e27c68db4ab431c5892747e6
sia7997 <sia7997@naver.com>
2023-11-12 12:55:41 +0900

    Build : 프로젝트 설정 변경

--- presentation/src/main/java/com/pat/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/pat/presentation/ui/MainActivity.kt
@@ -7,6 +7,5 @@ import com.pat.presentation.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
