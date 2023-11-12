6fed6e1d8b9df3bf2c277f987cd2ad65210c2045
sia7997 <sia7997@naver.com>
2023-11-12 09:16:21 +0900

    Feat : 프로젝트 초기화

--- presentation/src/main/java/com/pat/presentation/BlankViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.pat.presentation
-
-import androidx.lifecycle.ViewModel
-
-class BlankViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
---
+++ presentation/src/main/java/com/pat/presentation/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pat.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pat.presentation.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
