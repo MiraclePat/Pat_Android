33d165ec518db23dc2620ef9a0cf2a8885df6347
sia7997 <sia7997@naver.com>
2023-11-17 16:01:34 +0900

    Feat : 공고글 상세 screen 초안

--- presentation/src/main/java/com/pat/presentation/ui/pat/PatDetailScreen.kt
+++ presentation/src/main/java/com/pat/presentation/ui/pat/PatDetailScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -27,6 +28,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -40,17 +42,29 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.pat.presentation.R
+import com.pat.presentation.ui.home.components.CategoryButton
+import com.pat.presentation.ui.theme.Gray200
+import com.pat.presentation.ui.theme.Gray400
+import com.pat.presentation.ui.theme.Gray50
+import com.pat.presentation.ui.theme.Gray500
+import com.pat.presentation.ui.theme.Gray700
+import com.pat.presentation.ui.theme.Primary50
+import com.pat.presentation.ui.theme.PrimaryMain
+import com.pat.presentation.ui.theme.Typography
+import com.skydoves.landscapist.glide.GlideImage
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -62,13 +76,11 @@ fun PostDetailView(modifier: Modifier = Modifier) {
         modifier = modifier
             .fillMaxSize(),
         topBar = {
-            TopAppBar(
-                modifier = Modifier.fillMaxWidth(),
+            CenterAlignedTopAppBar(
                 title = {
                     Text(
                         text = "강아지 산책",
-                        fontSize = 14.sp,
-                        modifier = modifier.fillMaxWidth(),
+                        fontSize = 14.sp, //TODO STYLE
                     )
                 },
                 navigationIcon = {
@@ -80,13 +92,19 @@ fun PostDetailView(modifier: Modifier = Modifier) {
                     }
                 },
                 actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_write),
+                            contentDescription = "Write"
+                        )
+                    }
                 },
             )
         }
-    ) { values ->
+    ) { innerPadding ->
         Column(
             modifier = Modifier
-                .padding(values)
+                .padding(innerPadding)
                 .verticalScroll(scrollState),
         ) {
             PostDetailScreen()
@@ -129,21 +147,25 @@ fun PostDetailScreen(modifier: Modifier = Modifier) {
                 modifier = modifier.padding(3.dp)
             )
         }
-        Text("서울시 관악구 신사동", modifier = modifier.padding(3.dp))
-        Text("11월 13일(월) - 11월 27일(월) 총 2주간", modifier = modifier.padding(3.dp))
-        Text("현재 8명 / 20명", modifier = modifier.padding(3.dp)) //TODO GETSTRING
+        Spacer(modifier.height(16.dp))
+        PatSimpleInfo()
+        Spacer(modifier.size(20.dp))
+        UserInfo()
+        Box(modifier = modifier.fillMaxWidth().height(10.dp).background(Gray50))
         Spacer(modifier.size(20.dp))
 
-        Text("인증사진", fontSize = 16.sp, modifier = modifier.padding(3.dp))
-        PatPhotos()
+        //TODO 펼쳐보기 만들어야 함
+        Text("팟 상세정보", fontSize = 16.sp, modifier = modifier.padding(3.dp))
+//        PatPhotos()
         Spacer(modifier.size(20.dp))
+        Box(modifier = modifier.fillMaxWidth().height(10.dp).background(Gray50))
 
         Text(
-            "팟 정보", fontSize = 16.sp, modifier = modifier
+            "팟 소개", fontSize = 16.sp, modifier = modifier
                 .padding(3.dp)
                 .fillMaxWidth()
         )
-        CustomText("강아지와 주 2회 산책해요", painterResource(id = R.drawable.ic_add))
+        CustomText("강아지와 주 2회 산책해요", painterResource(id = R.drawable.ic_chat_dot))
 
         Spacer(modifier.size(20.dp))
 
@@ -152,24 +174,126 @@ fun PostDetailScreen(modifier: Modifier = Modifier) {
 
         Spacer(modifier.size(20.dp))
         Text("인증 빈도", fontSize = 16.sp, modifier = modifier.padding(3.dp))
-        ProofFrequencyList()
+        CategoryButtonList()
 
         Spacer(modifier.size(20.dp))
-        Text("시작일-종료일", fontSize = 16.sp, modifier = modifier.padding(3.dp))
+
+        Text("시작일 - 종료일", fontSize = 16.sp, modifier = modifier.padding(3.dp))
+        DateText(painter = painterResource(id = R.drawable.ic_calendar))
+        Spacer(modifier.size(20.dp))
+
+        Box(modifier = modifier.fillMaxWidth().height(10.dp).background(Gray50))
 
         Spacer(modifier.size(20.dp))
         Text("인증 방법", fontSize = 16.sp, modifier = modifier.padding(3.dp))
-        Box(modifier.padding(10.dp)) {
-//                    CustomText("목줄을 찬 반려동물이 바깥 풍경과 함꼐 나오도록 사진을 찍어주세요.", "")
-        }
+        CustomText(
+            "목줄을 찬 반려동물이 바깥 풍경과 함꼐 나오도록 사진을 찍어주세요.",
+            painterResource(id = R.drawable.ic_chat_check)
+        )
+
 
         Spacer(modifier.size(20.dp))
         Text("인증 수단", fontSize = 16.sp, modifier = modifier.padding(3.dp))
-//                CustomText("실시간 촬영", "")
-//                CustomText("갤러리에서 사진 가져오기", "")
+        CustomText("실시간 촬영", painterResource(id = R.drawable.ic_camera))
+        Spacer(modifier.height(7.dp))
+
+        CustomText("갤러리에서 사진 가져오기", painterResource(id = R.drawable.ic_gallery))
 
         Spacer(modifier.size(44.dp))
         ParticipatePatButton()
+    }
+}
+
+@Composable
+fun UserInfo(
+    modifier: Modifier = Modifier,
+    userName : String ="유저 닉네임"
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        //유저이미지정보
+        Text(userName, style = Typography.labelLarge, color = Gray700, fontSize = 15.sp, modifier = modifier.padding(end = 6.dp))
+        Text("개설자", style = Typography.displaySmall, color = Gray500, fontSize = 12.sp)
+    }
+
+}
+
+@Composable
+fun PatSimpleInfo(
+    modifier: Modifier = Modifier,
+    location: String = "서울시 관악구 신사동",
+    startDate: String = "12.5(금)",
+    endDate: String = "12.25(금)",
+    nowPerson: Int = 8,
+    maxPerson: Int = 10,
+    textColor: Color = Color(0xFF009D65),
+) {
+
+    Column() {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(tint= Gray400 ,painter = painterResource(id = R.drawable.ic_map), contentDescription = null)
+            Spacer(modifier.width(6.dp))
+            Text(text = location)
+        }
+        Spacer(modifier.height(8.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(tint= Gray400 ,
+                painter = painterResource(id = R.drawable.ic_calendar),
+                contentDescription = null
+            )
+            Spacer(modifier.width(6.dp))
+            Text(text = "$startDate - $endDate")
+        }
+        Spacer(modifier.height(8.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(tint= Gray400 ,painter = painterResource(id = R.drawable.ic_user), contentDescription = null)
+            Spacer(modifier.width(6.dp))
+            Text(text = "현재 $nowPerson / $maxPerson")
+        }
+    }
+
+}
+
+@Composable
+fun DateText(
+    startDate: String? = "11월 13일(월)",
+    endDate: String? = "11월 27일(월)",
+    painter: Painter,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .border(
+                BorderStroke(1.dp, Gray200),
+                shape = RoundedCornerShape(4.dp)
+            )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+        ) {
+            Icon(
+                painter = painter, contentDescription = null, tint = PrimaryMain
+            )
+
+            Spacer(modifier = modifier.width(8.dp))
+            Row() {
+                Text(text = "시작", color = PrimaryMain)
+                Text(text = "${startDate}")
+                Spacer(modifier = modifier.width(12.dp))
+                Text(text = "종료", color = PrimaryMain)
+                Text(text = "${endDate}")
+            }
+        }
     }
 }
 
@@ -182,17 +306,18 @@ fun CustomText(
     Box(
         modifier = modifier
             .border(
-                BorderStroke(1.dp, Color.Gray),
+                BorderStroke(1.dp, Gray200),
                 shape = RoundedCornerShape(4.dp)
             )
     ) {
         Row(
+            verticalAlignment = Alignment.CenterVertically,
             modifier = modifier
                 .fillMaxWidth()
                 .padding(10.dp),
         ) {
             Icon(
-                painter = painter, contentDescription = null
+                painter = painter, contentDescription = null, tint = PrimaryMain
             )
 
             Spacer(modifier = modifier.width(8.dp))
@@ -207,25 +332,13 @@ fun ParticipatePatButton(
 ) {
     Box(
         modifier = modifier
+            .fillMaxWidth()
+            .height(46.dp)
             .clickable(onClick = {})
+            .clip(RoundedCornerShape(4.dp))
+            .background(color = PrimaryMain), contentAlignment = Alignment.Center
     ) {
-        Column {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(
-                    text = buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                textDecoration = TextDecoration.Underline,
-                            ),
-                        ) {
-                            Text("팟 참여하기")
-                        }
-                    },
-                    color = Color.Red,
-                    fontSize = 16.sp,
-                )
-            }
-        }
+        Text("팟 참여하기", color = Color.White, fontSize = 16.sp)
     }
 }
 
@@ -251,64 +364,50 @@ fun PreviewPhotos(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun ProofFrequencyList() {
-    val categories = listOf<String>("월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일")
-    LazyRow() {
-        items(5) {
-            categories.forEach { category ->
-                ProofFrequencyButton(text = category)
+fun CategoryButtonList(stateList: String? = null) {
+    // stateList는 서버에서 가져온다
+    val days = listOf<String>("월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일")
+    val proofDays = listOf<String>("월요일", "목요일", "토요일")
+
+    Column {
+        // First row (월요일 to 금요일)
+        Row(
+        ) {
+            days.take(5).forEach { day ->
+                val categoryButton = if (day in proofDays) {
+                    CategoryButton(
+                        text = day,
+                        textColor = PrimaryMain,
+                        border = PrimaryMain,
+                        buttonColor = Primary50
+                    )
+                } else {
+                    CategoryButton(text = day)
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+            }
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Row(
+        ) {
+            days.takeLast(2).forEach { day ->
+                val categoryButton = if (day in proofDays) {
+                    CategoryButton(
+                        text = day,
+                        textColor = PrimaryMain,
+                        border = PrimaryMain,
+                        buttonColor = Primary50
+                    )
+                } else {
+                    CategoryButton(text = day)
+                }
+                Spacer(modifier = Modifier.width(8.dp))
             }
         }
     }
-}
+    Spacer(Modifier.size(10.dp))
 
-
-@Composable
-fun ProofFrequencyButton(
-    modifier: Modifier = Modifier,
-    onClick: () -> Unit = {},
-    buttonColor: Color = Color.Black,
-    text: String,
-    textColor: Color = Color.White,
-    enabled: Boolean = true,
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    onPressing: () -> Unit = {},
-    onPressed: () -> Unit = {}
-) {
-    val isPressed by interactionSource.collectIsPressedAsState()
-
-    val verticalPadding by animateDpAsState(
-        targetValue = if (isPressed) 13.dp else 16.dp, label = ""
-    )
-
-    LaunchedEffect(isPressed) {
-        if (isPressed) onPressing()
-        else onPressed()
-    }
-
-    Button(
-        modifier = modifier,
-        interactionSource = interactionSource,
-        shape = RoundedCornerShape(12.dp),
-        contentPadding = PaddingValues(vertical = verticalPadding),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = buttonColor
-        ),
-        enabled = enabled,
-        onClick = {
-            onClick()
-        },
-    ) {
-        Box(
-        ) {
-            Text(
-                modifier = Modifier.align(Alignment.Center),
-                style = MaterialTheme.typography.bodyMedium,
-                text = text,
-                color = textColor
-            )
-        }
-    }
 }
 
 
--- presentation/src/main/java/com/pat/presentation/ui/theme/Type.kt
+++ presentation/src/main/java/com/pat/presentation/ui/theme/Type.kt
@@ -1,31 +1,50 @@
 package com.pat.presentation.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.sp
 import com.pat.presentation.R
 
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.pretendard_bold)),
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        color = PrimaryMain,
+    ),
+    displaySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.pretendard_medium)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 13.sp,
+        color = Gray700,
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily(Font(R.font.pretendard_black)),
+        fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
         fontWeight = FontWeight.SemiBold,
         fontSize = 16.sp,
-        color = Gray900
+        color = Gray900,
     ),
     labelMedium = TextStyle(
-        fontFamily = FontFamily(Font(R.font.pretendard_black)),
+        fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
         fontWeight = FontWeight.SemiBold,
         fontSize = 14.sp,
-        color = Gray900
+        color = Gray900,
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(Font(R.font.pretendard_medium)),
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 12.sp,
+        color = Gray700,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.pretendard_medium)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        color = Gray700,
     )
 )
