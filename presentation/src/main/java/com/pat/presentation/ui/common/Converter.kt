9819dc3b46d202a7573ff432affd5b307eba4f27
lgy15 <lgy159@naver.com>
2023-11-20 16:40:07 +0900

    Feature : TimePicker 구현 완료, 디자인은 추후 수정

--- presentation/src/main/java/com/pat/presentation/ui/common/Converter.kt
+++ presentation/src/main/java/com/pat/presentation/ui/common/Converter.kt
@@ -5,7 +5,7 @@ import java.util.Date
 import java.util.Locale
 
 
-val convertMillisToDate: (Long) -> (String) = {millis ->
+val convertMillisToDate: (Long) -> (String) = { millis ->
     val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
     formatter.format(Date(millis))
 }
@@ -22,12 +22,22 @@ val convertDateFormat: (String) -> (String) = { inputDateString ->
 }
 
 val convertTimeFormat: (String) -> (String) = { inputDateString ->
-    val outputFormat = SimpleDateFormat("MM월 dd일", Locale.getDefault())
+    val outputFormat = SimpleDateFormat("hh:mm:00", Locale.getDefault())
     val date = if (inputDateString.isNotEmpty()) {
-        val inputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-        inputFormat.parse(inputDateString)!!
+        val inputFormat = SimpleDateFormat("a h시 m분", Locale.getDefault())
+        inputFormat.parse(inputDateString.replace("오전", "AM").replace("오후", "PM"))!!
     } else {
         Date()
     }
     outputFormat.format(date)
+}
+
+fun convertTimeText(input: String): String {
+    val inputFormat = SimpleDateFormat("a:hh:mm", Locale.getDefault())
+    val outputFormat = SimpleDateFormat("a h시 m분", Locale.getDefault())
+
+    val date: Date = inputFormat.parse(input) ?: return "잘못된 형식"
+
+    return outputFormat.format(date).replace("AM", "오전")
+        .replace("PM", "오후")
 }
