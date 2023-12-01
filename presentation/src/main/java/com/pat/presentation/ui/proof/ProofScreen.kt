6a39d1d663d292810caecd37e07c7a54b510c4a7
lgy15 <lgy159@naver.com>
2023-12-01 10:50:23 +0900

    Feat : 참여 취소 구현

--- presentation/src/main/java/com/pat/presentation/ui/proof/ProofScreen.kt
+++ presentation/src/main/java/com/pat/presentation/ui/proof/ProofScreen.kt
@@ -110,7 +110,7 @@ fun ProofScreenView(
                     )
                 },
                 navigationIcon = {
-                    IconButton(onClick = { /*TODO*/ }) {
+                    IconButton(onClick = { navController.popBackStack() }) {
                         Icon(
                             imageVector = ImageVector.vectorResource(R.drawable.ic_back_arrow),
                             contentDescription = "Go back"
@@ -157,7 +157,18 @@ fun ProofScreen(
     val proofBitmap by viewModel.proofBitmap.collectAsState() //인증사진
     val bottomSheetState by viewModel.bottomSheetState.collectAsState()
 
-
+    LaunchedEffect(content) {
+        Logger.t("MainTest").i("${content}")
+    }
+    @Composable
+    fun finalButton(text: String) {
+        FinalButton(
+            text = text,
+            textColor = PrimaryMain,
+            backColor = Gray300,
+            stokeColor = Gray300
+        )
+    }
 
     Column(
         modifier = modifier
@@ -429,7 +440,29 @@ fun ProofScreen(
                 isAll = "전체"
             )
         }
-        FinalButton(text = "인증하기", onClick = { showBottomSheet = true })
+
+        when (content.state) {
+            "CANCELABLE" -> {
+                FinalButton(text = "팟 취소하기", onClick = { viewModel.withdrawPat(content.patId) })
+            }
+
+            "NO_CANCELABLE" -> {
+                finalButton("시작 하루 전이에요!")
+            }
+
+            "IN_PROGRESS" -> {
+                if (content.isCompleted) {
+                    finalButton("인증하기 (이미 인증했어요!)")
+                } else {
+                    FinalButton(text = "인증하기", onClick = { showBottomSheet = true })
+                }
+            }
+
+            "COMPLETED" -> {
+                finalButton("이미 종료된 팟이에요!")
+            }
+        }
+
         if (showBottomSheet || bottomSheetState) {
             ModalBottomSheet(
                 onDismissRequest = {
@@ -455,17 +488,20 @@ fun ProofScreen(
                         navController = navController,
                         bitmap = proofBitmap,
                         viewModel = viewModel,
-                        realTime = true
+                        realTime = !content.realtime
                     )
 
                     Spacer(modifier.padding(bottom = 32.dp))
 
 
                     SelectButton(
-                        text = "인증하기",
+                        text = "이 사진으로 인증하기",
                         onClick = {
-//                            val proofImageByte = viewModel.
-//                            viewModel.proofPat()
+                            Logger.t("MainTest").i("${content.isCompleted}")
+                            Logger.t("MainTest").i("${viewModel.proofImageBytes}")
+                            val proofImageByte = viewModel.proofImageBytes
+                            viewModel.proofPat(proofImageByte)
+                            showBottomSheet = false
                         },
                         backColor = if (proofBitmap == null) Gray300 else PrimaryMain,
                         textColor = if (proofBitmap == null) White else White,
@@ -592,7 +628,7 @@ fun PercentBar(modifier: Modifier = Modifier, success: Int, fail: Int, all: Int)
         )
         Box(
             modifier
-                .fillMaxWidth((all - success - fail) / all.toFloat())
+                .fillMaxWidth((all - success - fail) / all.toFloat() - 0.01f) // 부동 소수점 ^^
                 .fillMaxHeight()
                 .clip(
                     if (success == 0 && fail == 0) RoundedCornerShape(100.dp) else RoundedCornerShape(
--- presentation/src/main/java/com/pat/presentation/ui/proof/ProofViewModel.kt
+++ presentation/src/main/java/com/pat/presentation/ui/proof/ProofViewModel.kt
@@ -13,6 +13,7 @@ import com.pat.domain.model.proof.ProofContent
 import com.pat.domain.model.proof.ProofRequestInfo
 import com.pat.domain.usecase.image.GetByteArrayByUriUseCase
 import com.pat.domain.usecase.member.GetParticipatingDetailUseCase
+import com.pat.domain.usecase.pat.WithdrawPatUseCase
 import com.pat.domain.usecase.proof.GetMyProofUseCase
 import com.pat.domain.usecase.proof.GetSomeoneProofUseCase
 import com.pat.domain.usecase.proof.ProofPatUseCase
@@ -44,6 +45,7 @@ class ProofViewModel @Inject constructor(
     private val getSomeoneProofUseCase: GetSomeoneProofUseCase,
     private val getParticipatingDetailUseCase: GetParticipatingDetailUseCase,
     private val getByteArrayByUriUseCase: GetByteArrayByUriUseCase,
+    private val withdrawPatUseCase: WithdrawPatUseCase,
 ) : ViewModel() {
     private val patId = savedStateHandle.get<Long?>(
         key = "patId"
@@ -55,7 +57,7 @@ class ProofViewModel @Inject constructor(
     private val _proofBitmap = MutableStateFlow<Bitmap?>(null)
     val proofBitmap = _proofBitmap.asStateFlow()
 
-    private var proofImageBytes: ByteArray = ByteArray(0)
+    var proofImageBytes: ByteArray = ByteArray(0)
 
     private val _uiState = MutableStateFlow(ParticipatingUiState())
     val uiState: StateFlow<ParticipatingUiState> = _uiState.asStateFlow()
@@ -123,7 +125,6 @@ class ProofViewModel @Inject constructor(
         }
     }
 
-
     fun getSomeoneProof() {
         viewModelScope.launch {
             val result = getSomeoneProofUseCase(patId, ProofRequestInfo())
@@ -132,6 +133,17 @@ class ProofViewModel @Inject constructor(
                 _proofs.emit(ProofUiState(content = content))
             } else {
                 Logger.t("MainTest").i("${proofs}")
+            }
+        }
+    }
+
+    fun withdrawPat(patId: Long) {
+        viewModelScope.launch {
+            val result = withdrawPatUseCase(patId)
+            if (result.isSuccess) {
+                result.getOrThrow()
+            } else {
+                Logger.t("MainTest").i("취소 실패")
             }
         }
     }
