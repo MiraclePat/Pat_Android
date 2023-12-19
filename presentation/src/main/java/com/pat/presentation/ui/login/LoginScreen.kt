4fa90d549a25045e9a8d2b6a215f4b4c54247f84
sia7997 <sia7997@naver.com>
2023-12-19 14:57:55 +0900

    Feat : 회원탈퇴기능

--- presentation/src/main/java/com/pat/presentation/ui/login/LoginScreen.kt
+++ presentation/src/main/java/com/pat/presentation/ui/login/LoginScreen.kt
@@ -19,6 +19,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -40,7 +42,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @Composable
 fun LoginScreenView(
     modifier: Modifier = Modifier,
-    loginState: MutableState<Boolean>,
+    loginState: MutableState<Boolean> = remember { mutableStateOf(false) },
     viewModel: LoginViewModel = hiltViewModel()
 ) {
 
